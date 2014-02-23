ee6653601a5a3f3d7d39d49a21012204eb077e11
VanceKingSaxbeA <vsaxbe@yahoo.com>
2014-02-23 23:31:45 +0530

    31455 th/nd times commit by Vance King Saxbe. A and development team Power Dominion Enterprise

--- src/goldsaxanalytics.py
+++ src/goldsaxanalytics.py
@@ -1,4 +1,4 @@
-import numpy as np
+""" Copyright (c) <2014> Author Vance King Saxbe. A, and contributors Power Dominion Enterprise, Precieux Consulting and other contributors. Modelled, Architected and designed by Vance King Saxbe. A. with the geeks from GoldSax Consulting and GoldSax Technologies email @vsaxbe@yahoo.com. Development teams from Power Dominion Enterprise, Precieux Consulting. Project sponsored by GoldSax Foundation, GoldSax Group and executed by GoldSax Manager."""import numpy as np
 import sqlite3 as lite
 import re
 from numpy import warnings
